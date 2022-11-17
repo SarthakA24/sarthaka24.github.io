c32548903a3fc6904a5b9236955a68e44121559b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-17 21:14:12 +0530

    initial commit

---
+++ assets/js/custom-scripts.js
@@ -0,0 +1,118 @@
+(function($) {
+  "use strict";
+  
+    $.fn.andSelf = function() {
+      return this.addBack.apply(this, arguments);
+    }
+  
+      /* Loader Code Start */
+      $(window).on("load", function() { 
+          $(".section-loader").fadeOut("slow");
+          
+          var $container = $('.portfolioContainer');
+          $container.isotope({
+              filter: '*',
+              animationOptions: {
+                  queue: true
+              }
+          });
+       
+          $('.portfolio-nav li').click(function(){
+              $('.portfolio-nav .current').removeClass('current');
+              $(this).addClass('current');
+       
+              var selector = $(this).attr('data-filter');
+              $container.isotope({
+                  filter: selector,
+                  animationOptions: {
+                      queue: true
+                  }
+               });
+               return false;
+          });
+        });
+      /* Loader Code End */
+  
+   
+    /*
+    |====================
+    | Mobile NAv trigger
+    |=====================
+    */
+    
+    var trigger = $('.navbar-toggler'),
+      overlay     = $('.overlay'),
+      navc     = $('.navbar-collapse'),
+      active      = false;
+  
+
+      $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
+          $('.navbar-toggler').toggleClass('active')
+        //   $('#js-navbar-menu').toggleClass('active');
+        //   $('.navbar-collapse').toggleClass('show');
+          overlay.toggleClass('active');
+          navc.toggleClass('active');
+      });  
+      
+        
+    /*
+    |=================
+    | Onepage Nav
+    |================
+    */
+        
+      $('#mh-header').onePageNav({
+          currentClass: 'active', 
+          changeHash: false,
+          scrollSpeed: 750,
+          scrollThreshold: 0.5,
+      });
+    
+    /*
+    |=================
+    | fancybox
+    |================
+    */
+ 
+      $("[data-fancybox]").fancybox({});
+      
+      
+    /*
+    |===============
+    | WOW ANIMATION
+    |==================
+    */
+    	var wow = new WOW({
+          mobile: false  // trigger animations on mobile devices (default is true)
+      });
+      wow.init();
+    
+  
+    /*
+    | ==========================
+    | NAV FIXED ON SCROLL
+    | ==========================
+    */
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".nav-scroll").addClass("nav-strict");
+        } else {
+            $(".nav-scroll").removeClass("nav-strict");
+        }
+    });
+    
+
+    /*
+    |=================
+    | Progress bar
+    |================
+    */   
+    $(".determinate").each(function(){
+      var width = $(this).text();
+      $(this).css("width", width)
+        .empty()
+        .append('<i class="fa fa-circle"></i>');                
+    });
+    
+}(jQuery));

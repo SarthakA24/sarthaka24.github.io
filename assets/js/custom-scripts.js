ef519ec456652631998debb5bf1e24b84cb44e64
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-21 21:22:59 +0530

    reformatted the js

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,74 +1,51 @@
-(function($) {
+(function ($) {
   "use strict";
-  
-    $.fn.andSelf = function() {
-      return this.addBack.apply(this, arguments);
+
+  $.fn.andSelf = function () {
+    return this.addBack.apply(this, arguments);
+  }
+
+  /* Loader Code Start */
+  $(window).on("load", function () {
+    $(".section-loader").fadeOut("slow");
+  });
+
+  /* Mobile Nav Trigger */
+  var trigger = $('.navbar-toggler'),
+    overlay = $('.overlay'),
+    navc = $('.navbar-collapse'),
+    active = false;
+
+
+  $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
+    $('.navbar-toggler').toggleClass('active')
+    overlay.toggleClass('active');
+    navc.toggleClass('active');
+  });
+
+  /* Onepage Nav */
+  $('#mh-header').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+  });
+
+  /* WOW ANIMATION */
+  var wow = new WOW({
+    mobile: true
+  });
+  wow.init();
+
+
+  /* NAV FIXED ON SCROLL */
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $(".nav-scroll").addClass("nav-strict");
+    } else {
+      $(".nav-scroll").removeClass("nav-strict");
     }
-  
-      /* Loader Code Start */
-      $(window).on("load", function() { 
-          $(".section-loader").fadeOut("slow");
-        });
-      /* Loader Code End */
-  
-   
-    /* Mobile Nav Trigger */
-    var trigger = $('.navbar-toggler'),
-      overlay     = $('.overlay'),
-      navc     = $('.navbar-collapse'),
-      active      = false;
-  
+  });
 
-      $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
-          $('.navbar-toggler').toggleClass('active')
-          overlay.toggleClass('active');
-          navc.toggleClass('active');
-      });  
- 
-    /* Onepage Nav */
-      $('#mh-header').onePageNav({
-          currentClass: 'active', 
-          changeHash: false,
-          scrollSpeed: 750,
-          scrollThreshold: 0.5,
-      });
-      
-    /*
-    |===============
-    | WOW ANIMATION
-    |==================
-    */
-    	var wow = new WOW({
-          mobile: true  // trigger animations on mobile devices (default is true)
-      });
-      wow.init();
-    
-  
-    /*
-    | ==========================
-    | NAV FIXED ON SCROLL
-    | ==========================
-    */
-    $(window).on('scroll', function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 50) {
-            $(".nav-scroll").addClass("nav-strict");
-        } else {
-            $(".nav-scroll").removeClass("nav-strict");
-        }
-    });
-    
-
-    /*
-    |=================
-    | Progress bar
-    |================
-    */   
-    // $(".determinate").each(function(){
-    //   var width = $(this).text();
-    //   $(this).css("width", width)
-    //     .empty()
-    //     .append('<i class="fa fa-circle"></i>');                
-    // });
-    
 }(jQuery));

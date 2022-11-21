0558ccb262c4235f4ba6396c8d06d2118bc31b55
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-21 21:21:56 +0530

    commented unnecessary js code

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -8,38 +8,11 @@
       /* Loader Code Start */
       $(window).on("load", function() { 
           $(".section-loader").fadeOut("slow");
-          
-          var $container = $('.portfolioContainer');
-          $container.isotope({
-              filter: '*',
-              animationOptions: {
-                  queue: true
-              }
-          });
-       
-          $('.portfolio-nav li').click(function(){
-              $('.portfolio-nav .current').removeClass('current');
-              $(this).addClass('current');
-       
-              var selector = $(this).attr('data-filter');
-              $container.isotope({
-                  filter: selector,
-                  animationOptions: {
-                      queue: true
-                  }
-               });
-               return false;
-          });
         });
       /* Loader Code End */
   
    
-    /*
-    |====================
-    | Mobile NAv trigger
-    |=====================
-    */
-    
+    /* Mobile Nav Trigger */
     var trigger = $('.navbar-toggler'),
       overlay     = $('.overlay'),
       navc     = $('.navbar-collapse'),
@@ -48,34 +21,17 @@
 
       $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
           $('.navbar-toggler').toggleClass('active')
-        //   $('#js-navbar-menu').toggleClass('active');
-        //   $('.navbar-collapse').toggleClass('show');
           overlay.toggleClass('active');
           navc.toggleClass('active');
       });  
-      
-        
-    /*
-    |=================
-    | Onepage Nav
-    |================
-    */
-        
+ 
+    /* Onepage Nav */
       $('#mh-header').onePageNav({
           currentClass: 'active', 
           changeHash: false,
           scrollSpeed: 750,
           scrollThreshold: 0.5,
       });
-    
-    /*
-    |=================
-    | fancybox
-    |================
-    */
- 
-      $("[data-fancybox]").fancybox({});
-      
       
     /*
     |===============
@@ -83,7 +39,7 @@
     |==================
     */
     	var wow = new WOW({
-          mobile: false  // trigger animations on mobile devices (default is true)
+          mobile: true  // trigger animations on mobile devices (default is true)
       });
       wow.init();
     
@@ -108,11 +64,11 @@
     | Progress bar
     |================
     */   
-    $(".determinate").each(function(){
-      var width = $(this).text();
-      $(this).css("width", width)
-        .empty()
-        .append('<i class="fa fa-circle"></i>');                
-    });
+    // $(".determinate").each(function(){
+    //   var width = $(this).text();
+    //   $(this).css("width", width)
+    //     .empty()
+    //     .append('<i class="fa fa-circle"></i>');                
+    // });
     
 }(jQuery));
